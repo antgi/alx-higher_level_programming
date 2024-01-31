196e9a268c0b24035303955d031b8d09f9ae8cf3
antgi <anlebu05@gmail.com>
2024-01-31 11:32:35 -0800

    Commiting rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This defines a rectangle class."""
+"""Defines a Rectangle class."""
+
+
 class Rectangle:
-"""Represernts a rectanagle."""
+    """Represent a rectangle."""
     pass
